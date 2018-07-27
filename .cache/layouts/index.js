580573ea3937ddb16b5249ca4ede7f04356ad141
ibis0503 <kennyhong@outlook.com>
2018-07-27 00:09:21 -0700

    .gitignore fix

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/The99missedcalls/Documents/my-app/src/layouts/index.js"
+  import data from "/Users/The99missedcalls/Documents/my-app/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
