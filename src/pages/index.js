6e1e96e4ece046d72735218300784894364eff81
ibis0503 <kennyhong@outlook.com>
2018-07-27 00:42:07 -0700

    completed basic css

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Kenny Hong</h1>
-    <p>We can change this.</p>
-    
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Kenny Hong</h1>
+        <p>We can change this.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>    
+    </div>
   </div>
 )
 
