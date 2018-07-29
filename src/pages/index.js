97af7c929b34a0e9e4ea970f37d3f99f87e5fc0b
ibis0503 <kennyhong@outlook.com>
2018-07-28 23:34:39 -0700

    interactions and animation on page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>Learn to design and code React apps</h1>
+        <h1>Learn to <br /> design and code React apps</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
         <Link to="/page-2/">Go to page 2</Link>
       </div>    
